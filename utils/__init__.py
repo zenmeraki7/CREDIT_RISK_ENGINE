278c03ceadf49b2a134a9b5c5a06f897c1c4afd3
zenmeraki7 <zenmerakiofficial@gmail.com>
2026-03-09 11:30:23 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,22 @@
+# """
+# Utils package for Credit Risk Assessment
+# """
+
+# from .pdf_generator import generate_decision_pdf
+
+# __all__ = ['generate_decision_pdf']
+
+
 """
 Utils package for Credit Risk Assessment
 """
 
-from .pdf_generator import generate_decision_pdf
+from .pdf_generator import generate_decision_pdf, generate_audit_pdf
+from .ocr_extractor import extract_cibil_from_pdf, infer_categorical_flags
 
-__all__ = ['generate_decision_pdf']
+__all__ = [
+    'generate_decision_pdf',
+    'generate_audit_pdf',
+    'extract_cibil_from_pdf',
+    'infer_categorical_flags',
+]
