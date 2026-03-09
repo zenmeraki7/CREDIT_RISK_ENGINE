e9add103791675a0a04752c14c92a12f2b31241e
zenmeraki7 <zenmerakiofficial@gmail.com>
2026-03-09 11:30:59 +0530

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,11 +1,27 @@
 
+# """
+# Utils package for Credit Risk Assessment
+# """
+
+# # from .pdf_generator import generate_decision_pdf
+# # #>>>>>> bfbad9f (Added stage2 CIBIL model and feature importance)
+# # __all__ = ['generate_decision_pdf']
+
+# from .pdf_generator import generate_decision_pdf, generate_audit_pdf
+# __all__ = ['generate_decision_pdf', 'generate_audit_pdf']
+
+
+
 """
 Utils package for Credit Risk Assessment
 """
 
-# from .pdf_generator import generate_decision_pdf
-# #>>>>>> bfbad9f (Added stage2 CIBIL model and feature importance)
-# __all__ = ['generate_decision_pdf']
-
 from .pdf_generator import generate_decision_pdf, generate_audit_pdf
-__all__ = ['generate_decision_pdf', 'generate_audit_pdf']
+from .ocr_extractor import extract_cibil_from_pdf, infer_categorical_flags
+
+__all__ = [
+    'generate_decision_pdf',
+    'generate_audit_pdf',
+    'extract_cibil_from_pdf',
+    'infer_categorical_flags',
+]
